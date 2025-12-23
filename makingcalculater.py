7fe55acb94834b70bdd4ca1a29ac4f0d651855a9
Ashish yaduveer <ashishyaduveer791@gmail.com>
2025-12-23 14:15:05 +0530

    Add files via upload

---
+++ makingcalculater.py
@@ -0,0 +1,19 @@
+num1 = eval(input("Enter a Firts number:"))
+num2  = eval(input("Enter a Second number:"))
+Opr = eval(input("Enter The Opr.."))
+if Opr =="+":
+    print(num1+num2)
+elif Opr== "*":
+    print(num1*num2)
+elif Opr== "-":
+    print(num1-num2)
+elif Opr ==" /":
+    print(num1 / num2)
+else:
+    
+
+    print("Invalid input")
+
+
+
+           
