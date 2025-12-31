7bcd71f9bf2cd5012fbf6a65885b33d0c20d73e9
Ashish yaduveer <ashishyaduveer791@gmail.com>
2025-12-31 22:39:32 +0530

    Add files via upload

---
+++ day3question3.py
@@ -0,0 +1,23 @@
+# a= int(input("Enter the number"))
+# b = int(input("Enter power number"))
+# squareroot = a*b
+# print(squareroot)        
+
+
+#  using Expanentiation
+num  = 64
+num1 = int(input("Enter a number here:"))
+sr = num1**(1/2)
+print("The square root of the given numberr is", sr)
+
+
+
+#  Another Method using math module
+import math
+number = int(input("Enter a number:"))
+sr = math.sqrt(number)
+print("The square root of the given number is",sr)
+
+
+
+
