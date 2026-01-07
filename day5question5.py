ccc6af681723f7e2d3defcf0d3486537db814a61
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-01-07 10:41:34 +0530

    Add files via upload

---
+++ day5question5.py
@@ -0,0 +1,21 @@
+# solving  question using  third variable
+# x = 5
+# y =6
+# temp = x 
+# x=y
+# y= temp
+# print("the vakue of y is",y)
+
+
+
+
+#  Another method
+
+x =12
+y=15
+
+x,y = y,x
+
+
+print("The value of x is",x)
+print("The values of y is",y)
