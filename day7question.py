3e3ecc04ab598b0404d4c203448609fed3ca2a99
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-01-11 15:05:25 +0530

    Add files via upload

---
+++ day7question.py
@@ -0,0 +1,25 @@
+# m= int(input("Enter a number:"))
+# if m>0:
+#     print("it is a positive number")
+# elif m ==0:
+#     print("it is zero")
+# else:
+#     print("Itt is a negative number")
+
+
+    #  fuction using
+# def check_number(num):
+#         if num>0:
+#             return "positive"
+#         if num ==0:
+#             return " Zero"
+#         else: 
+#             return "negative"
+#         num = int(input( "Enter a number:"))
+        # print(check_number(num))
+                  
+            # pattern  matching
+num = int(input("Enter a number:"))
+
+
+
