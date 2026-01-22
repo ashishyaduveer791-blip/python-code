6e4d1b011082c2807e7cea0f46aeeda467c8ca07
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-01-22 15:18:14 +0530

    Add files via upload

---
+++ day10question10.py
@@ -0,0 +1,17 @@
+num1 =  int(input('Enter a number here:'))
+
+num2 =  int(input('Enter a number here:'))
+
+num3 =  int(input('Enter a number here:'))
+if (num1>num2) and (num1>num3):
+    print(num1,"is a largest number:-")
+elif (num2>num1) and (num2>num3):
+    print(num2, "is the largest number")
+else:
+    print("num3,is the largest number")
+    
+
+    
+    
+   
+    
---
+++ day12questtion12.py
@@ -0,0 +1,8 @@
+# To genirated to random number
+# import random
+# x = random.randint(1,25)
+# print(x)
+# x = int(input("Roll user dice"))
+import random
+x = random.randint(1,6)
+print(x)
