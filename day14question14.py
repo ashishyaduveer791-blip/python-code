acf0b753993ba570c714ee9bd1bc6cbe5d163d7e
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-01-24 17:10:52 +0530

    Add files via upload

---
+++ day14question14.py
@@ -0,0 +1,28 @@
+# calcuate the cobvert into ferheit  to celcuies
+# celcius= int(input("Enter the temprature i->"))
+# ferahehgth = (celcius*9/5)+32
+# print(ferahehgth)
+
+# x =20
+# if x>0:
+#  print('x is positive')
+# x = 5
+# y = 6
+# if x>y:
+#     # print(x)
+#     # print(y)
+
+
+# x =3
+# if x < 10:
+#     print('small')
+#     print('Done')
+#  chail condition
+if 0<x:
+    if x <10:
+        print('x is a positive single-digit number.')
+
+
+
+
+
