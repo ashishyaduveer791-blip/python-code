af76a6ca48586f9abe7d3fae398591a52ebbdd84
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-01-29 14:17:15 +0530

    Add files via upload

---
+++ day16question.py
@@ -0,0 +1,42 @@
+a =0
+b = 1
+ 
+# num=int(input("Entera number->"))
+# if num == 1:
+#     print(a)
+# else:
+#     print(a)
+#     print(b)
+
+# for i in range(1,num+1):
+#     c = a+b
+#     a = b
+#     b = print(c)
+
+# Iterative Method
+# n= int(input("Entera  number:"))
+# a,b=0,1
+# for i in range(n):
+#     print(a, end=" ")
+#     a, b = b, a+b
+
+# def fib(n=(int(input("Enter a number->")))):
+#     if n<=1:
+#         return n
+#     return fib(n-1) +fib(n-2)
+# for i in  range(10):
+#     print(fib(i),end=" ")
+
+
+    #  using loop
+n = int(input("Enternumber:"))
+    a,b = 0,1
+    i =  0
+    while i <n:
+        print(a,end=" ")
+        a, b = a+b
+        i += 1
+
+
+
+
