911539723e25d3d51e1a26a1adf10adf97cdd80f
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-02-08 00:04:14 +0530

    Add files via upload

---
+++ day22quesstion.py
@@ -0,0 +1,12 @@
+for i in range(1,100):
+    if i %13 ==0:
+        print(i)
+
+        #  solution 2   using lamba fuction on filter
+        l =[39,48,26,33,67,87]
+        result - list(fiter(lambda x:x%13==0,l))
+        print('the number divisibae by 13 are:')
+        
+
+
+        
