7bfaca08926f0f44ac93f62e584bf97c22360896
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-03-30 22:35:58 +0530

    Add files via upload

---
+++ question48.py
@@ -0,0 +1,12 @@
+# how to check if list emptty in python
+my_list = []
+if not my_list:
+    print("The list is empty")
+
+    my_list = []
+    if len(my_list)==0:
+        print("the list is empty")
+        # how solutioon 3 using
+        my_list = [ 12,20,30]
+        if my_list == []:
+            print("The list is empty")
