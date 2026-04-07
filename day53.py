e37fce63c76a35762f31eff67516ba56801642b2
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-04-07 22:55:14 +0530

    Add files via upload

---
+++ day53.py
@@ -0,0 +1,9 @@
+friends = {
+
+    "Ashish":"RIse","Monica": "Chandler","shudam": "joe"
+}
+name = input("Enter a key here:")
+if name in friends.keys():
+    print("it is present")
+
+    # progrm to check if a alreadfy present
