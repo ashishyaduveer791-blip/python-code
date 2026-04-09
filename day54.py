530c40aa3c350bc79be27c56e8b470da540531ee
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-04-09 23:29:19 +0530

    Add files via upload

---
+++ day54.py
@@ -0,0 +1,20 @@
+# how to parse string  to a floating  using python code
+# solution 1
+string = " 124568"
+print(type(string))
+int_str = int(string)
+print(type(int_str))
+
+# sloution parse string into floot
+
+string = "123.45"
+print(type(string))
+print(string)
+float_string = float(string)
+print (type(float_string))
+# soution 3
+string=("123.45")
+print(type(string))
+str_float_int =(float(string))
+print(type(str_float_int))
+
