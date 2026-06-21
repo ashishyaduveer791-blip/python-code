922042c901d5fd7bd532d205ae5c46b8d3b1d2fc
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-06-21 11:08:04 +0530

    Add files via upload

--- question1.py
+++ question1.py
@@ -1,9 +1,81 @@
-# Scan n values in range 0-3 and print the number of times each value has occurred.
-n = input("Enter a number:")
-# create empty list 
-numbers =[]
-for i in range(n):
-    num=int(input("Enter value(0,9):"))
-    numbers.appened(num)
-    for i in range(0,10):
-        print(i,"occured",numbers.count(i),"time")
+# class Array: ## we crete a blueprint  called class
+#     def __init__(self):  # run automatley whwn object is create
+#         self.ashish=[]  # we create an empty list inside object for storage the data
+#         # insert the function
+#         def insert(index,Value):
+#             print("Array:",self.ashish)
+
+#             a= Array()
+#             n=int(input("How many element?"))
+#             for i in range(n):
+#                 val=int(input("Enter Element:"))
+#                 a.ashish.append(val)
+#                 print("Before insertion:")
+#                 a.display()
+
+#                 index=int(input('Enter index to insert:'))
+#                 Value=int(input('Enter a values to insert:'))
+#                 a.insert(index,Value)
+#                 print("After insertion:")
+#                 a.display()
+        
+         
+
+# class Array:
+#     def __init__(self):
+#         self.arr = []
+
+#     def insert(self, index, value):
+#         self.arr.insert(index, value)
+
+#     def display(self):
+#         print("Array:", self.arr)
+
+
+# a = Array()
+
+# n = int(input("How many elements? "))
+
+# for i in range(n):
+#     val = int(input("Enter element: "))
+#     a.arr.append(val)
+
+# print("Before insertion:")
+# a.display()
+
+# index = int(input("Enter index to insert: "))
+# value = int(input("Enter value to insert: "))
+
+# a.insert(index, value)
+
+# print("After insertion:")
+# a.display()
+
+
+class home:
+    def __init__(self):
+        self.ashish=[]
+        def insert(self,index,value):
+            self.self.insert(index,value)
+            def dispaly(self):
+                print("Array:",self.ashish)
+
+                a=array()
+                n=int(input("how mmany element?"))
+                for i in range(n):
+                    val=int(input("Enter element:"))
+                    a.arr.append(val)
+                    print("before inserrtion:")
+                    a.display()
+                    index=int(input("ENter index to insert:"))
+                    value=int(input("enter values to innsert:"))
+                    a.insert(index,value)
+                    print("After insertion:")
+                    a.display()
+
+
+                
+            
+
+
+    
